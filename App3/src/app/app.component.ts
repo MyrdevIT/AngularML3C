7ca65ce84eed977e212f8f823b7edae9bb26701a
MyrdevIT <myrdevit@gmail.com>
2026-03-25 16:51:17 +0100

    App3: add an amazing feature

--- App3/src/app/app.component.ts
+++ App3/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
