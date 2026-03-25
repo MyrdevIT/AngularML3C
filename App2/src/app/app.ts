843815c816ce0a55c07d99ea61c70625fd991b46
MyrdevIT <myrdevIT@gmail.com>
2026-03-25 08:02:08 +0100

    add accordion item

--- App2/src/app/app.ts
+++ App2/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Article } from './article/article';
 import { AppFooter } from './app-footer/app-footer';
 import { AppHeader } from './app-header/app-header';
+import { Accordion } from './accordion/accordion';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Article, AppFooter, AppHeader ],
+  imports: [RouterOutlet, Article, AppFooter, AppHeader, Accordion ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
